e1219a41c77f077dc32fe6bd9371a834434650c1
surya <suryaprk165@gmail.com>
2025-01-31 09:53:48 +0530

    d

---
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Login from "../../Component/login/login";
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  )
+}
+
+export default LoginPage
